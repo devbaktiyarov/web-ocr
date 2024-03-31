0dcf14b0396904030b8d37688f997dabd27ce9a0
Arsen Baktiyarov <devbaktiyarov@gmail.com>
2024-03-31 21:34:03 +0600

    basic functionality done

---
+++ src/main/java/com/devbaktiyarov/webocr/controller/TessOcrController.java
@@ -0,0 +1,55 @@
+package com.devbaktiyarov.webocr.controller;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/v1")
+public class TessOcrController {
+    
+    
+    private final Tesseract tesseract;
+
+
+    public TessOcrController(Tesseract tesseract) {
+        this.tesseract = tesseract;
+    }
+
+
+
+
+    @PostMapping("/")
+    private String convertImage(@RequestParam(name = "image") MultipartFile file) throws TesseractException, IOException {
+        String result = tesseract.doOCR(createImageFromBytes(file.getBytes())); // анализ текста и превращение его в данные
+        System.out.println(result);
+        return "";
+    }
+
+    private BufferedImage createImageFromBytes(byte[] imageData) {
+        ByteArrayInputStream bais = new ByteArrayInputStream(imageData);
+        try {
+            return ImageIO.read(bais);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+}
---
+++ src/test/java/com/devbaktiyarov/webocr/WebOcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbaktiyarov.webocr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebOcrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
