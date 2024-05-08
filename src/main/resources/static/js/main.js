1331ad08807216ff401e6bd3410ea8f91f725c10
Arsen Baktiyarov <devbaktiyarov@gmail.com>
2024-05-09 00:17:31 +0600

    image to text function done

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,93 @@
+var input = document.querySelector("#file-input");
+var output = document.querySelector("#file-output");
+var submitButton = document.querySelector("#submit-button");
+var progressBar = document.querySelectorAll("#progress-status");
+
+var browseButton = document.querySelector("#browse-button");
+var dropArea = document.querySelector("#drop-area");
+
+let imagesArray = [];
+let dataTransfer = new DataTransfer()
+
+browseButton.onclick = () => {
+    input.click();
+};
+
+
+submitButton.addEventListener("click", showProgress);
+
+dropArea.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    dropArea.classList.add("active");
+});
+
+dropArea.addEventListener("dragleave", () => {
+    dropArea.classList.remove("active");
+});
+
+
+// !!!!! get rid off duplicating
+dropArea.addEventListener("drop", (e) => {
+    e.preventDefault();
+    dropArea.classList.remove("active");
+    // input.files = e.dataTransfer.files;
+    console.log("Dropped");
+    uploadFiles(e.dataTransfer.files);
+    const files = input.files
+    for (let i = 0; i < files.length; i++) {
+        console.log(files[0]);
+        imagesArray.push(files[i])
+    }
+    dropArea.classList.remove("active");
+    displayImages();  
+
+    if(imagesArray.length != 0) {
+        submitButton.disabled = false;
+    }
+    imagesArray = []; 
+});
+
+function uploadFiles(files) {
+    for (let i = 0; i < files.length; i++) {
+        dataTransfer.items.add(files[i])
+    }
+
+    input.files = dataTransfer.files
+    console.log(input.files)
+}
+
+
+input.addEventListener("change", () => {
+    const files = input.files;
+    for (let i = 0; i < files.length; i++) {
+        console.log(files[0]);
+        imagesArray.push(files[i])
+    }
+    displayImages();  
+
+    if(imagesArray.length != 0) {
+        submitButton.disabled = false;
+    }
+    imagesArray = []; 
+});
+
+
+function displayImages() {
+    let images = ""
+    imagesArray.forEach((image, index) => {
+        images += `<div class="col-4">
+                    <img src="${URL.createObjectURL(image)}" class="img-fluid border" alt="image">
+                    <div id="progress-status" class="progress d-none my-2">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
+                    </div>
+                </div>`
+    })
+    output.innerHTML = images;
+}
+
+function showProgress() {
+    progressBars.forEach(p => p.classList.remove("d-none"));
+}
+
+
+
---
+++ src/main/resources/static/js/result.js
@@ -0,0 +1,11 @@
+// var copyButtons = document.querySelectorAll("#copy-button");
+// var copyTextDetails = document.querySelectorAll(".card-text");
+
+// copyButtons.forEach(function(copyButton, index){
+//     copyButton.addEventListener("click", () => {
+//         // Не поддерживается без HTTTPS
+//         navigator.clipboard.writeText(copyTextDetails[index].innerText);
+//         alert("Text copied")
+//     });
+// });
+
